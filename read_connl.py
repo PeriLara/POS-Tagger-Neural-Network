7adb63afbcbca426fc73458ed2a22f6f16318cfe
Lara Perinetti <perinettilara16@gmail.com>
2018-06-09 09:30:53 +0200

    New read_connl

--- read_connl.py
+++ read_connl.py
@@ -51,9 +51,7 @@ def read_conllu(filename, window_size, voc=None):
                     voc.append(f"d{i}")
                     voc.append(f"f{i}")
 
-        voc2index = {x:i for i,x in enumerate(voc)}
-
-        return sentences, voc2index, voc
+        return sentences, voc
 
     except IOError:
         print("Problème lors de la lecture du fichier")
@@ -66,45 +64,8 @@ def get_classes(sentences):
             classes.add(c)
     return classes
 
-def create_onehots(vocabulary, classes):
-    """ Etape 1 :
-            Crée des vecteurs creux pour les classes et le vocabulaire donnés
-            retourne les vecteurs creux
-    """
-    nbr_words = len(vocabulary)
-    X_onehots = np.zeros(shape=(nbr_words, nbr_words))
 
-    nbr_classes = len(classes)
-    Y_onehots = np.zeros(shape=(nbr_classes, nbr_classes))
 
-    for i,_ in enumerate(vocabulary):
-        X_onehots[i][i] = 1
-    for j,_ in enumerate(classes):
-        Y_onehots[j][j] = 1
-
-    return X_onehots, Y_onehots
-
-def create_features(sentences, X_onehots, vocabulary, Y_onehots, window, voc2index, classe2index):
-    X = []
-    y = []
-    for sentence in sentences:
-        longueur_phrase = len(sentence[0])
-        for i, word_tag in enumerate(zip(sentence[0], sentence[1])):
-            word, tag = word_tag
-            x = np.zeros(shape=((window*2+1), len(vocabulary)))
-            j = 0
-            if word in vocabulary:
-                index_word = voc2index[word]
-            else:
-                index_word = voc2index[UNKNOWN]
-            for w in range(-window+1, window):
-                if i + w < 0:   x[j][voc2index[f"d{w}"]] = 1
-                elif i + w >= longueur_phrase:  x[j][voc2index[f"f{w}"]] = 1
-                else:   x[j][index_word] = 1
-                j+=1
-            X.append(x)
-            y.append(Y_onehots[classe2index[tag]])
-    return X, y
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "Corpus au format sgm")
@@ -166,6 +127,48 @@ def write_txt(filename_input, filename_output):
             fichier.write(mot)
 
     print(f"Fichier {filename_output} est écrit")
+
+
+def create_features(sentences, X_onehots, vocabulary, Y_onehots, window, voc2index, classe2index):
+    X = []
+    y = []
+    for sentence in sentences:
+        longueur_phrase = len(sentence[0])
+        for i, word_tag in enumerate(zip(sentence[0], sentence[1])):
+            word, tag = word_tag
+            x = np.zeros(shape=((window*2+1), len(vocabulary)))
+            j = 0
+            if word in vocabulary:
+                index_word = voc2index[word]
+            else:
+                index_word = voc2index[UNKNOWN]
+            for w in range(-window+1, window):
+                if i + w < 0:   x[j][voc2index[f"d{w}"]] = 1
+                elif i + w >= longueur_phrase:  x[j][voc2index[f"f{w}"]] = 1
+                else:   x[j][index_word] = 1
+                j+=1
+            X.append(x)
+            y.append(Y_onehots[classe2index[tag]])
+    return X, y
+
+
+def create_onehots(vocabulary, classes):
+    """ Etape 1 :
+            Crée des vecteurs creux pour les classes et le vocabulaire donnés
+            retourne les vecteurs creux
+    """
+    nbr_words = len(vocabulary)
+    X_onehots = np.zeros(shape=(nbr_words, nbr_words))
+
+    nbr_classes = len(classes)
+    Y_onehots = np.zeros(shape=(nbr_classes, nbr_classes))
+
+    for i,_ in enumerate(vocabulary):
+        X_onehots[i][i] = 1
+    for j,_ in enumerate(classes):
+        Y_onehots[j][j] = 1
+
+    return X_onehots, Y_onehots
 """ 
 
 
